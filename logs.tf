9af43f7db8916a405bc30e504005f9e8685badd0
Jeremy Wallez <jeremy.wallez@gmail.com>
2023-05-02 13:15:02 +0200

    fix: remove useless s3 buckets for log (need refactoring)

--- logs.tf
+++ logs.tf
@@ -5,15 +5,3 @@ resource "aws_cloudwatch_log_group" "log_group" {
     Type : "logs"
   })
 }
-
-resource "aws_s3_bucket" "alb_logs_bucket" {
-  bucket = "${var.name}-alb-logs"
-  tags = merge(var.default_tags, {
-    Type : "logs"
-  })
-}
-
-resource "aws_s3_bucket_acl" "alb_log_bucket_acl" {
-  bucket = aws_s3_bucket.alb_logs_bucket.id
-  acl    = "private"
-}
